67020baf4b6c40939a75e9fdb0ad39604179a40f
Eric Mika <eric@ericmika.com>
2024-04-18 18:15:35 -0400

    Add readonly test option to limit risk to your local contacts list.

--- test/module.spec.js
+++ test/module.spec.js
@@ -10,6 +10,7 @@ const {
   requestAccess,
 } = require('../index')
 
+const isReadOnly = process.env.TEST_READONLY ?? false
 const isCI = require('is-ci')
 const ifit = (condition) => (condition ? it : it.skip)
 const ifdescribe = (condition) => (condition ? describe : describe.skip)
@@ -60,7 +61,7 @@ describe('node-mac-contacts', () => {
   })
 
   describe('addNewContact(contact)', () => {
-    it('throws if contact is not a nonempty object', () => {
+    ifit(!isReadOnly)('throws if contact is not a nonempty object', () => {
       expect(() => {
         addNewContact(1)
       }).to.throw(/contact must be a non-empty object/)
@@ -70,7 +71,7 @@ describe('node-mac-contacts', () => {
       }).to.throw(/contact must be a non-empty object/)
     })
 
-    it('should throw if name properties are not strings', () => {
+    ifit(!isReadOnly)('should throw if name properties are not strings', () => {
       expect(() => {
         addNewContact({ firstName: 1 })
       }).to.throw(/firstName must be a string/)
@@ -84,29 +85,32 @@ describe('node-mac-contacts', () => {
       }).to.throw(/nickname must be a string/)
     })
 
-    it('should throw if birthday is not a string in YYYY-MM-DD format', () => {
-      expect(() => {
-        addNewContact({ birthday: 1 })
-      }).to.throw(/birthday must be a string/)
+    ifit(!isReadOnly)(
+      'should throw if birthday is not a string in YYYY-MM-DD format',
+      () => {
+        expect(() => {
+          addNewContact({ birthday: 1 })
+        }).to.throw(/birthday must be a string/)
 
-      expect(() => {
-        addNewContact({ birthday: '01-01-1970' })
-      }).to.throw(/birthday must use YYYY-MM-DD format/)
-    })
+        expect(() => {
+          addNewContact({ birthday: '01-01-1970' })
+        }).to.throw(/birthday must use YYYY-MM-DD format/)
+      },
+    )
 
-    it('should throw if phoneNumbers is not an array', () => {
+    ifit(!isReadOnly)('should throw if phoneNumbers is not an array', () => {
       expect(() => {
         addNewContact({ phoneNumbers: 1 })
       }).to.throw(/phoneNumbers must be an array/)
     })
 
-    it('should throw if emailAddresses is not an array', () => {
+    ifit(!isReadOnly)('should throw if emailAddresses is not an array', () => {
       expect(() => {
         addNewContact({ emailAddresses: 1 })
       }).to.throw(/emailAddresses must be an array/)
     })
 
-    ifit(!isCI)('should successfully add a contact', () => {
+    ifit(!isCI && !isReadOnly)('should successfully add a contact', () => {
       const success = addNewContact({
         firstName: 'William',
         lastName: 'Grapeseed',
@@ -142,31 +146,34 @@ describe('node-mac-contacts', () => {
       }).to.throw(errorMessage)
     })
 
-    ifit(!isCI)('should retrieve a contact by name predicates', () => {
-      addNewContact({
-        firstName: 'Sherlock',
-        lastName: 'Holmes',
-        nickname: 'Sherllock',
-        birthday: '1854-01-06',
-        phoneNumbers: ['+1234567890'],
-        emailAddresses: ['sherlock@holmes.com'],
-      })
+    ifit(!isCI && !isReadOnly)(
+      'should retrieve a contact by name predicates',
+      () => {
+        addNewContact({
+          firstName: 'Sherlock',
+          lastName: 'Holmes',
+          nickname: 'Sherllock',
+          birthday: '1854-01-06',
+          phoneNumbers: ['+1234567890'],
+          emailAddresses: ['sherlock@holmes.com'],
+        })
 
-      const contacts = getContactsByName('Sherlock Holmes')
-      expect(contacts).to.be.an('array').of.length.gte(1)
+        const contacts = getContactsByName('Sherlock Holmes')
+        expect(contacts).to.be.an('array').of.length.gte(1)
 
-      const contact = contacts[0]
-      expect(contact.firstName).to.eql('Sherlock')
-    })
+        const contact = contacts[0]
+        expect(contact.firstName).to.eql('Sherlock')
+      },
+    )
   })
 
   describe('deleteContact({ name, identifier })', () => {
-    it('should throw if name is not a string', () => {
+    ifit(!isReadOnly)('should throw if name is not a string', () => {
       expect(() => {
         deleteContact({ name: 12345 })
       }).to.throw(/name must be a string/)
     })
-    it('should throw if identifier is not a string', () => {
+    ifit(!isReadOnly)('should throw if identifier is not a string', () => {
       expect(() => {
         deleteContact({ identifier: 12345 })
       }).to.throw(/identifier must be a string/)
@@ -174,7 +181,7 @@ describe('node-mac-contacts', () => {
   })
 
   describe('updateContact(contact)', () => {
-    it('throws if contact is not a nonempty object', () => {
+    ifit(!isReadOnly)('throws if contact is not a nonempty object', () => {
       expect(() => {
         updateContact(1)
       }).to.throw(/contact must be a non-empty object/)
@@ -184,7 +191,7 @@ describe('node-mac-contacts', () => {
       }).to.throw(/contact must be a non-empty object/)
     })
 
-    it('should throw if name properties are not strings', () => {
+    ifit(!isReadOnly)('should throw if name properties are not strings', () => {
       expect(() => {
         updateContact({ firstName: 1 })
       }).to.throw(/firstName must be a string/)
@@ -198,23 +205,26 @@ describe('node-mac-contacts', () => {
       }).to.throw(/nickname must be a string/)
     })
 
-    it('should throw if birthday is not a string in YYYY-MM-DD format', () => {
-      expect(() => {
-        updateContact({ birthday: 1 })
-      }).to.throw(/birthday must be a string/)
+    ifit(!isReadOnly)(
+      'should throw if birthday is not a string in YYYY-MM-DD format',
+      () => {
+        expect(() => {
+          updateContact({ birthday: 1 })
+        }).to.throw(/birthday must be a string/)
 
-      expect(() => {
-        updateContact({ birthday: '01-01-1970' })
-      }).to.throw(/birthday must use YYYY-MM-DD format/)
-    })
+        expect(() => {
+          updateContact({ birthday: '01-01-1970' })
+        }).to.throw(/birthday must use YYYY-MM-DD format/)
+      },
+    )
 
-    it('should throw if phoneNumbers is not an array', () => {
+    ifit(!isReadOnly)('should throw if phoneNumbers is not an array', () => {
       expect(() => {
         updateContact({ phoneNumbers: 1 })
       }).to.throw(/phoneNumbers must be an array/)
     })
 
-    it('should throw if emailAddresses is not an array', () => {
+    ifit(!isReadOnly)('should throw if emailAddresses is not an array', () => {
       expect(() => {
         updateContact({ emailAddresses: 1 })
       }).to.throw(/emailAddresses must be an array/)
@@ -241,7 +251,7 @@ describe('node-mac-contacts', () => {
       }).to.throw(/An observer is already observing/)
     })
 
-    it('emits an event when the contact is changed', (done) => {
+    ifit(!isReadOnly)('emits an event when the contact is changed', (done) => {
       listener.setup()
 
       addNewContact({
